9f6e7d176cdc3459083c82306dfa14e5d66a18c8
Jacob Cox <jacobcox2006@gmail.com>
2025-06-17 19:35:48 +0100

    Cyclic Compound Support

--- src/main/kotlin/uk/co/jcox/chemvis/application/moleditor/actions/BondOrderAction.kt
+++ src/main/kotlin/uk/co/jcox/chemvis/application/moleditor/actions/BondOrderAction.kt
@@ -5,7 +5,8 @@ import org.joml.Vector3f
 import org.joml.minus
 import org.joml.times
 import uk.co.jcox.chemvis.application.chemengine.IMoleculeManager
-import uk.co.jcox.chemvis.application.moleditor.GhostImplicitHydrogenGroupComponent
+import uk.co.jcox.chemvis.application.moleditor.LevelMolLinkUtil
+import uk.co.jcox.chemvis.application.moleditor.LevelViewUtil
 import uk.co.jcox.chemvis.application.moleditor.MolIDComponent
 import uk.co.jcox.chemvis.application.moleditor.NewOrganicEditorState
 import uk.co.jcox.chemvis.cvengine.scenegraph.EntityLevel
@@ -28,23 +29,30 @@ class BondOrderAction (
 
         val structBond = molManager.getJoiningBond(structMolecule.molID, structAtomA.molID, structAtomB.molID)
 
-        updateStruct(molManager, structMolecule.molID, structBond)
-        updateLevel(level)
+        if (structBond != null) {
+            updateStructForBondOrderChange(molManager, structMolecule.molID, structBond)
+            updateLevelForBondOrderChange(level)
 
-        molManager.recalculate(structMolecule.molID)
-        replaceOldLabels(molManager, structMolecule.molID, structAtomA.molID, levelAtomA)
-        replaceOldLabels(molManager, structMolecule.molID, structAtomB.molID, levelAtomB)
+            molManager.recalculate(structMolecule.molID)
+            replaceOldLabels(molManager, structMolecule.molID, structAtomA.molID, levelAtomA)
+            replaceOldLabels(molManager, structMolecule.molID, structAtomB.molID, levelAtomB)
 
+            return structMolecule.molID
+        }
+
+        //Then the struct bond is null so we can just form a normal bond between the two atoms
+        val newStructBond = updateStructForNewBond(molManager, structMolecule.molID, structAtomA.molID, structAtomB.molID)
+        updateLevelForNewBond(levelMolecule, levelAtomA, levelAtomB, newStructBond)
         return structMolecule.molID
     }
 
 
 
-    private fun updateStruct(molManager: IMoleculeManager, structMolecule: UUID, structBond: UUID) {
+    private fun updateStructForBondOrderChange(molManager: IMoleculeManager, structMolecule: UUID, structBond: UUID) {
         molManager.updateBondOrder(structMolecule, structBond, 2)
     }
 
-    private fun updateLevel(level: EntityLevel) {
+    private fun updateLevelForBondOrderChange(level: EntityLevel) {
         //We need to add another bond that is just a small distance perpendicular to the bond direction
 
         //1) Get the bond direction
@@ -60,5 +68,16 @@ class BondOrderAction (
         val newBondEntity = level.addEntity()
         newBondEntity.addComponent(TransformComponent(orthVec.x, orthVec.y, orthVec.z))
         newBondEntity.addComponent(LineDrawerComponent(levelAtomA.id, levelAtomB.id))
+    }
+
+
+    private fun updateStructForNewBond(molManager: IMoleculeManager, structMolecule: UUID, atomA: UUID, atomB: UUID) : UUID {
+        val newBond = molManager.formBond(structMolecule, atomA, atomB, 1)
+        return newBond
+    }
+
+    private fun updateLevelForNewBond(mol: EntityLevel, atomA: EntityLevel, atomB: EntityLevel, structLink: UUID) {
+        val bond = LevelViewUtil.createBond(mol, atomA, atomB)
+        LevelMolLinkUtil.linkObject(structLink, bond)
     }
 }
