f8537ad8fb2f96785d435cef638ffff29dd5b26d
JCOX <jacobcox2006@gmail.com>
2025-07-11 14:01:13 +0100

    Increase Version

--- src/main/kotlin/uk/co/jcox/chemvis/application/MolGLide.kt
+++ src/main/kotlin/uk/co/jcox/chemvis/application/MolGLide.kt
@@ -66,7 +66,7 @@ class MolGLide : IApplication, IInputSubscriber {
         const val GLOBAL_SCALE: Float = 0.1f
         const val SELECTION_MARKER_MESH: String = "SELECTION_MARKER_MESH"
         const val SELECTION_MARKER_MATERIAL: String = "SELECTION_MARKER_MATERIAL"
-        const val VERSION = "v0.0.2"
+        const val VERSION = "v0.0.4"
         const val WEBSITE = "https://github.com/JCox06/MolGLide/tree/master"
     }
 }
